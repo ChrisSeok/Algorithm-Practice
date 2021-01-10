a9aa53019a9250858393f9eb999078561c952c41
Lilo37 <76509117+Lilo37@users.noreply.github.com>
2021-01-10 21:19:27 +0900

    Update and rename Untitled3.cpp to 거스름돈_1.cpp
    Get change by coins <Using function and a global variable>

---
+++ 거스름돈_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int totalamount;   //전역변수 선언!! 
+   int GetChange(int coin){ //인자로 totalamount 넣으면 안되는 이유: 
+   //                          함수의 인자로 쓰는 순간 전역과 다른, 이름만 같은  새로운 변수가 되어버림 
+	int j=0;
+	for(int i=1;i<=totalamount;i++){
+   	if(i%coin==0) j++;	
+	}
+	cout<<coin<<"원:"<<j<<"개"; 
+    totalamount-=j*coin;
+} 
+
+
+int main(){
+	int change;//totalamount;
+	cin>>change;
+	totalamount=change;
+	cout<<"-------------"<<endl<<">>거스름돈:"<<endl; 
+	GetChange(500);
+	GetChange(100);
+	GetChange(50);
+	GetChange(10);
+	
+	
+	 
+} 
